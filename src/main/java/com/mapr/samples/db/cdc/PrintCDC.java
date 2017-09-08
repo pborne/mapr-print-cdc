23efa0aa822e2818a189071d5d68ccb568775e69
Paul Curtis <pcurtis@mapr.com>
2017-09-08 20:07:14 +0000

    Initial commit

---
+++ src/main/java/com/mapr/samples/db/cdc/PrintCDC.java
@@ -0,0 +1,72 @@
+package com.mapr.samples.db.cdc;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.ojai.FieldPath;
+import org.ojai.KeyValue;
+import org.ojai.Value;
+import org.ojai.store.cdc.ChangeDataRecord;
+import org.ojai.store.cdc.ChangeDataRecordType;
+import org.ojai.store.cdc.ChangeNode;
+import org.ojai.store.cdc.ChangeOp;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+import java.lang.System;
+
+public class PrintCDC {
+
+  private static String CHANGE_LOG = "/user/mapr/changelog:product_sales_tbl";
+
+  public static void main(String[] args) {
+
+    // Consumer configuration
+    Properties consumerProperties = new Properties();
+    consumerProperties.setProperty("group.id", "cdc.product_sales_tbl");
+    consumerProperties.setProperty("enable.auto.commit", "true");
+    consumerProperties.setProperty("auto.offset.reset", "latest");
+    consumerProperties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
+    consumerProperties.setProperty("value.deserializer", "com.mapr.db.cdc.ChangeDataRecordDeserializer");
+
+    // Consumer used to consume MapR-DB CDC events
+    KafkaConsumer<byte[], ChangeDataRecord> consumer = new KafkaConsumer<byte[], ChangeDataRecord>(consumerProperties);
+    consumer.subscribe(Arrays.asList(CHANGE_LOG));
+
+    while (true) {
+      ConsumerRecords<byte[], ChangeDataRecord> changeRecords = consumer.poll(500);
+      Iterator<ConsumerRecord<byte[], ChangeDataRecord>> iter = changeRecords.iterator();
+
+      while (iter.hasNext()) {
+        ConsumerRecord<byte[], ChangeDataRecord> crec = iter.next();
+        // The ChangeDataRecord contains all the changes made to a document
+        ChangeDataRecord changeDataRecord = crec.value();
+        Iterator<KeyValue<FieldPath, ChangeNode>> cdcNodes = changeDataRecord.iterator();
+        while (cdcNodes.hasNext()) {
+           Map.Entry<FieldPath, ChangeNode> changeNodeEntry = cdcNodes.next();
+           String fieldPathAsString = changeNodeEntry.getKey().asPathString();
+           ChangeNode changeNode = changeNodeEntry.getValue();
+           String documentId = changeDataRecord.getId().getString();
+
+           if (changeDataRecord.getType() == ChangeDataRecordType.RECORD_INSERT) {
+             System.out.println("Document Inserted " + documentId);
+           } else if (changeDataRecord.getType() == ChangeDataRecordType.RECORD_UPDATE) {
+             System.out.println("Document Updated " + documentId + "  FieldPath: '" + fieldPathAsString + "'");
+           } else if (changeDataRecord.getType() == ChangeDataRecordType.RECORD_DELETE) {
+             System.out.println("Document Deleted " + documentId);
+           }
+        }
+        System.out.println("---");
+      }
+    }
+
+  }
+
+}
